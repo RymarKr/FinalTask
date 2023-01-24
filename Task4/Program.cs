ce17a2a7f7f282592b76fc2b9e645e1b2c51b07f
Kirill <kirill.rymar@gmail.com>
2023-01-24 21:17:35 +0200

    first commit

---
+++ Task4/Program.cs
@@ -0,0 +1,27 @@
+﻿string[] arrayA = new string[6] {"I", "like", "geek",  "01", "brains", ":-)"};
+string[] arrayB = new string[arrayA.Length];
+
+SecondArrayWithIF(arrayA, arrayB);
+PrintArray(arrayB);
+
+void SecondArrayWithIF(string[] arrayA, string[] arrayB)
+{
+    int num = 0;
+    for (int i = 0; i < arrayA.Length; i++)
+    {
+    if(arrayA[i].Length <= 3)
+        {
+        arrayB[num] = arrayA[i];
+        num++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
